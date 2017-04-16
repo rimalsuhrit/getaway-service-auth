905aef478b29a01787c1096b93d6a917eb679800
suhrit <rimalsuhrit@gmail.com>
2017-04-16 08:10:48 -0400

    initial project structure

---
+++ src/main/java/getaway/service/auth/GetawayServiceAuthApplication.java
@@ -0,0 +1,12 @@
+package getaway.service.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetawayServiceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetawayServiceAuthApplication.class, args);
+	}
+}
